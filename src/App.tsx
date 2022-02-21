87446b61dc4b413f9ea4b56976bbb2371f9464cf
Iryna Rudzko <irinrudko@gmail.com>
2022-02-21 17:05:07 +0300

    refactor: divide logic and dumb components into 2 files

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,48 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Counter } from './counter/Counter';
+import { v1 } from 'uuid';
+
 
 const App = () => {
 
+  let initialValue = 0;
+  const [value, setValue] = useState<number>(initialValue)
+
+  const [buttonStatus1, setIncrementButton] = useState<boolean>(true)
+  const [buttonStatus2, setResetButton] = useState<boolean>(false)
+  const maxNum = 5;
+
+  const incHandler = () => {
+    setButtonStatus()
+    if (value < maxNum) {
+      setValue(value + 1)
+    }
+  }
+  const setButtonStatus = () => {
+    if (value === initialValue) {
+      setIncrementButton(false)
+    }
+    if (value === (maxNum - 1)) {
+      setResetButton(true)
+    }
+  }
+  const resetHandler = () => {
+    setValue(0)
+    setIncrementButton(true)
+    setResetButton(false)
+  }
+
   return (
     <div className="App">
-      <Counter />
+      <Counter
+        value={value}
+        maxNum={maxNum}
+        buttonStatus1={buttonStatus1}
+        buttonStatus2={buttonStatus2}
+        incHandler={incHandler}
+        resetHandler={resetHandler}
+      />
     </div>
   );
 }
--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,43 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './counter.module.css';
 
-export const Counter = () => {
-    let [value, setValue] = useState<number>(0)
-    let [buttonStatus1, setIncrementButton] = useState<boolean>(true)
-    let [buttonStatus2, setResetButton] = useState<boolean>(false)
-    const maxNum = 5;
+export type CounterType = {
+    value: number
+    maxNum: number
+    buttonStatus1: boolean
+    buttonStatus2: boolean
+    incHandler: () => void
+    resetHandler: () => void
+}
 
-
-    const incHandler = () => {
-
-        setButtonStatus()
-        if (value < maxNum) {
-            setValue(value + 1)
-        }
-    }
-
-    const setButtonStatus = () => {
-
-        if (value === 0) {
-            setIncrementButton(false)
-        }
-        if (value === (maxNum - 1)) {
-            setResetButton(true)
-        }
-    }
-
-    const resetHandler = () => {
-        setValue(0)
-        setIncrementButton(true)
-        setResetButton(false)
-    }
+export const Counter: React.FC<CounterType> = ({ value, maxNum, buttonStatus1, buttonStatus2, incHandler, resetHandler }) => {
     const ifMaxNum = (value === maxNum) ? style.max_num : ''
-
 
     return (
         <div className={style.item}>
             <div className={style.counter}>
-                <h1 className={`${ifMaxNum} ${style.head} `}>{value}</h1>
+                <h1 className={`${ifMaxNum} ${style.head}`}>{value}</h1>
                 <div className={style.body}>
                     <button disabled={buttonStatus1} onClick={resetHandler} className={style.button}>reset</button>
                     <button disabled={buttonStatus2} onClick={incHandler} className={style.button}>inc</button>
